21b32ffb0d5c523f7285a0d4eb545e341a0b262b
AlishaKausheen <alishakausheen@gmail.com>
2024-07-28 12:13:07 +0530

    Permute two arrays such that sum of every pair is greater or equal to K

---
+++ PermuteTwoArraysSumOfTwoPairsIsEqualOrGreaterThanK.java
@@ -0,0 +1,20 @@
+public class PermuteTwoArraysSumOfTwoPairsIsEqualOrGreaterThanK {
+    static boolean isPossible(Integer a[], int b[],
+                              int n, int k)
+    {
+
+        Arrays.sort(a, Collections.reverseOrder());
+
+
+        Arrays.sort(b);
+
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] + b[i] < k) {
+                return false;
+            }
+            return true;
+        }
+        return true;
+    }
+}
