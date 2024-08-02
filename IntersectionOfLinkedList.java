6ada1d7639b5ff314adba496add61ab48a21d7ba
AlishaKausheen <alishakausheen@gmail.com>
2024-08-02 21:01:10 +0530

    intersection of list

---
+++ IntersectionOfLinkedList.java
@@ -0,0 +1,27 @@
+public class IntersectionOfLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        while (headB != null) {
+            ListNode temp = headA;
+            while (temp != null) {
+                if (temp == headB) {
+                    return headB;
+                }
+                temp = temp.next;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
